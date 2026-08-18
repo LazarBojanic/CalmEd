3dee72f9da890ee3f2e0d987f352b257c30114cf
LazarBojanic <bojanic.lazar50@gmail.com>
2026-08-18 18:06:46 +0200

    fixed user deletion fk constraint error on payment

--- backend/calmed-backend/src/main/kotlin/com/calmed/calmedbackend/model/raw/payment/PaymentTable.kt
+++ backend/calmed-backend/src/main/kotlin/com/calmed/calmedbackend/model/raw/payment/PaymentTable.kt
@@ -1,13 +1,12 @@
 package com.calmed.calmedbackend.model.raw.payment
 
-import com.calmed.calmedbackend.model.raw.user.UserTable
 import org.jetbrains.exposed.v1.core.dao.id.java.UUIDTable
 import org.jetbrains.exposed.v1.core.java.javaUUID
 import org.jetbrains.exposed.v1.javatime.CurrentTimestamp
 import org.jetbrains.exposed.v1.javatime.timestamp
 
 object PaymentTable : UUIDTable(name = "payment") {
-	val userId = javaUUID("user_id").references(UserTable.id).nullable()
+	val userId = javaUUID("user_id").nullable()
 	val provider = enumerationByName("provider", 32, PaymentProvider::class)
 	// Google Play
 	val googlePurchaseToken = varchar("google_purchase_token", 1024).nullable()
