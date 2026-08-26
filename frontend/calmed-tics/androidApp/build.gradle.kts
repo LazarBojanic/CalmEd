43d6627b997cfb3e034e6afd6629ef3e9756307a
LazarBojanic <bojanic.lazar50@gmail.com>
2026-08-26 14:56:44 +0200

    update versionCode

--- frontend/calmed-tics/androidApp/build.gradle.kts
+++ frontend/calmed-tics/androidApp/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 		applicationId = "com.calmed.calmedtics"
 		minSdk = libs.versions.androidMinSdk.get().toInt()
 		targetSdk = libs.versions.androidTargetSdk.get().toInt()
-		versionCode = 10
+		versionCode = 11
 		versionName = "0.0.1"
 	}
 	packaging {
